4691b0b2abec0b459d8b48e37590a266b460d97a
Wiynxr <mkx.dake@gmail.com>
2023-01-09 02:30:24 +0300

    Добавил авторизацию

---
+++ src/redux/slices/auth.js
@@ -0,0 +1,65 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "../../axios";
+
+export const fetchAuth = createAsyncThunk(
+  "/auth/fetchUserData",
+  async (params) => {
+    const { data } = await axios.post("/auth/login", params);
+    return data;
+  }
+);
+
+export const fetchAuthMe = createAsyncThunk("/auth/fetchAuthMe", async () => {
+  const { data } = await axios.get("/auth/me");
+  return data;
+});
+
+const initialState = {
+  data: null,
+  status: "loading",
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    logout: (state) => {
+      localStorage.removeItem("token");
+      state.data = null;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchAuth.pending, (state) => {
+        state.status = "loading";
+        state.data = null;
+      })
+      .addCase(fetchAuth.fulfilled, (state, action) => {
+        state.status = "loaded";
+        state.data = action.payload;
+      })
+      .addCase(fetchAuth.rejected, (state) => {
+        state.status = "error";
+        state.data = null;
+      })
+
+      .addCase(fetchAuthMe.pending, (state) => {
+        state.status = "loading";
+        state.data = null;
+      })
+      .addCase(fetchAuthMe.fulfilled, (state, action) => {
+        state.status = "loaded";
+        state.data = action.payload;
+      })
+      .addCase(fetchAuthMe.rejected, (state) => {
+        state.status = "error";
+        state.data = null;
+      });
+  },
+});
+
+export const selectIsAuth = (state) => Boolean(state.auth.data);
+
+export const authReducer = authSlice.reducer;
+
+export const { logout } = authSlice.actions;
