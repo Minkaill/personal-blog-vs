c3cbae48afeed3837d2a6ad92093c866c075a36b
Wiynxr <mkx.dake@gmail.com>
2023-01-08 20:30:28 +0300

    Реализовал вывод и переход на динамические статьи

--- src/redux/slices/posts.js
+++ src/redux/slices/posts.js
@@ -1,10 +1,15 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import axios from '../../axios'
+import axios from "../../axios";
 
-export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
-    const { data } = await axios.get("/posts")
-    return data
-})
+export const fetchPosts = createAsyncThunk("posts/fetchPosts", async () => {
+  const { data } = await axios.get("/posts");
+  return data;
+});
+
+export const fetchTags = createAsyncThunk("tags/fetchTags", async () => {
+  const { data } = await axios.get("/tags");
+  return data;
+});
 
 const initialState = {
   posts: {
@@ -22,20 +27,33 @@ const postSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-      builder
+    builder
       .addCase(fetchPosts.pending, (state) => {
-          state.posts.items = []
-          state.posts.status = 'loading'
+        state.posts.items = [];
+        state.posts.status = "loading";
       })
       .addCase(fetchPosts.fulfilled, (state, action) => {
-          state.posts.items = action.payload
-          state.posts.status = 'loaded'
+        state.posts.items = action.payload;
+        state.posts.status = "loaded";
       })
       .addCase(fetchPosts.rejected, (state) => {
-          state.posts.items = []
-          state.posts.status = 'error'
+        state.posts.items = [];
+        state.posts.status = "error";
       })
-  }
+
+      .addCase(fetchTags.pending, (state) => {
+        state.tags.items = [];
+        state.tags.status = "loading";
+      })
+      .addCase(fetchTags.fulfilled, (state, action) => {
+        state.tags.items = action.payload;
+        state.tags.status = "loaded";
+      })
+      .addCase(fetchTags.rejected, (state) => {
+        state.tags.items = [];
+        state.tags.status = "error";
+      });
+  },
 });
 
 export const postsReducer = postSlice.reducer;
